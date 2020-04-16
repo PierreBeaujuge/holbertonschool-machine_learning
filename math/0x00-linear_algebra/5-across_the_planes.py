6e3a110a8f5ba7bbe29f22334ccd237f8d7fb021
PierreBeaujuge <pierre.beaujuge@gmail.com>
2020-04-16 20:03:43 +0000

    add None condition on array lens

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -9,7 +9,10 @@ def add_matrices2D(mat1, mat2):
     mat2 = [x[:] for x in mat2]
     # if matrix_shape(mat1) == matrix_shape(mat2):
     if len(mat1) == len(mat2):
-        return list(map(lambda arr1, arr2: add_arrays(arr1, arr2), mat1, mat2))
+        mat3 = list(map(lambda arr1, arr2: add_arrays(arr1, arr2), mat1, mat2))
+        if None in mat3:
+            return None
+        return mat3
     return None
 
 
