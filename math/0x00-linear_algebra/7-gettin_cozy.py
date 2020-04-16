9ef3e7058d51d44ba717e1a8f243b2b2616d6c8e
PierreBeaujuge <pierre.beaujuge@gmail.com>
2020-04-16 21:14:26 +0000

    clean up

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -7,8 +7,6 @@ def cat_matrices2D(mat1, mat2, axis=0):
     # making deep copies
     mat1 = [x[:] for x in mat1]
     mat2 = [x[:] for x in mat2]
-    # if len(matrix_shape(mat1)) != len(matrix_shape(mat2)):
-    #     return None
     # edge case not tested here:
     # if axis not in range(len(matrix_shape(mat1))):
     #     return None
@@ -23,19 +21,6 @@ def cat_matrices2D(mat1, mat2, axis=0):
 def cat_arrays(arr1, arr2):
     """function that adds two matrices element-wise"""
     return arr1 + arr2
-
-# def add_matrices2D(mat1, mat2):
-#     """function that adds two matrices element-wise"""
-#     if matrix_shape(mat1) == matrix_shape(mat2):
-#         return list(map(lambda arr1, arr2:
-#                      add_arrays(arr1, arr2), mat1, mat2))
-#     return None
-
-# def add_arrays(arr1, arr2):
-#     """function that adds two arrays element-wise"""
-#     if matrix_shape(arr1) == matrix_shape(arr2):
-#         return list(map(lambda x, y: x + y, arr1, arr2))
-#     return None
 
 
 def matrix_shape(matrix):
