1509b96b37623a9f4908277fc28d49f1d8809c6d
PierreBeaujuge <pierre.beaujuge@gmail.com>
2020-04-16 17:04:00 +0000

    clean up

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -4,17 +4,10 @@
 
 def add_arrays(arr1, arr2):
     """function that adds two arrays element-wise"""
-    # if matrix_shape(arr1) == matrix_shape(arr2):
     if len(arr1) == len(arr2):
         return list(map(lambda x, y: x + y, arr1, arr2))
+        # list comprehension is a working alternative;
+        # although both options return a new list:
         # return [arr1[i] + arr2[i] for i in range(len(arr1))]
     return None
 
-
-def matrix_shape(matrix):
-    """function that returns the shape of a matrix"""
-    shape = [len(matrix)]
-    while isinstance(matrix[0], list):
-        shape.append(len(matrix[0]))
-        matrix = matrix[0]
-    return shape
