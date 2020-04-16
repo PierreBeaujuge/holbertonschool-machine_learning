d8a403fadf5a3a56c733de37ebb6450f1c8b1439
PierreBeaujuge <pierre.beaujuge@gmail.com>
2020-04-16 15:30:25 +0000

    removed append()

--- math/0x00-linear_algebra/1-trim_me_down.py
+++ math/0x00-linear_algebra/1-trim_me_down.py
@@ -2,5 +2,5 @@
 matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
 the_middle = []
 size = len(matrix)
-the_middle.append([[matrix[i][2], matrix[i][3]] for i in range(size)])
+the_middle = [[matrix[i][2], matrix[i][3]] for i in range(size)]
 print("The middle columns of the matrix are: {}".format(the_middle))
