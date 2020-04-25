fd320852843ea2625a4bf23a2dd4def232b6a2e8
PierreBeaujuge <pierre.beaujuge@gmail.com>
2020-04-25 16:45:51 +0000

    added edge cases

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -7,4 +7,9 @@ import numpy as np
 
 def poly_derivative(poly):
     """function that calculates the derivative of a polynomial"""
-    return [poly[i] * i for i in range(1, len(poly))]
+    if poly and all(isinstance(x, (int, float)) for x in poly):
+        result = [poly[i] * i for i in range(1, len(poly))]
+        if not len(result):
+            return [0]
+        return result
+    return None
