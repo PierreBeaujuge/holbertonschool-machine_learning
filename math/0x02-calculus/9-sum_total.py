d64003d95f1ff9aa51d338c5a388dc08d413a360
PierreBeaujuge <pierre.beaujuge@gmail.com>
2020-04-25 16:05:31 +0000

    removed for loop

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -7,5 +7,6 @@ sum functions
 def summation_i_squared(n):
     """function that calculates summation_i_squared"""
     if n and isinstance(n, int):
-        return sum([i**2 for i in range(1, n + 1)])
+        # return sum([i**2 for i in range(1, n + 1)])
+        return int((n / 6) * (n + 1) * (2 * n + 1))
     return None
