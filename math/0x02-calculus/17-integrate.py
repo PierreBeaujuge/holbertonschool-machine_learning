ca687b257b535314ce778aed528665cd9855d428
PierreBeaujuge <pierre.beaujuge@gmail.com>
2020-04-25 17:06:51 +0000

    added poly is list

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -6,7 +6,8 @@ integration functions
 
 def poly_integral(poly, C=0):
     """function that calculates the integral of a polynomial"""
-    if poly and (isinstance(C, int) or isinstance(C, float)) and all(
+    if poly and isinstance(poly, list) and (
+            isinstance(C, int) or isinstance(C, float)) and all(
             isinstance(x, (int, float)) for x in poly):
         arr = [float(C)] + [poly[i] / (i + 1) for i in range(0, len(poly))]
         return [float(x) if not x.is_integer() else int(x) for x in arr]
