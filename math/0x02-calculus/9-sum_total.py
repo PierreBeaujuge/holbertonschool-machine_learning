ea47c6b50b4513ab4fe53d73f4116aa98ce5779f
PierreBeaujuge <pierre.beaujuge@gmail.com>
2020-04-25 15:21:51 +0000

    condition n is int

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -6,4 +6,6 @@ sum functions
 
 def summation_i_squared(n):
     """function that calculates summation_i_squared"""
-    return sum([i**2 for i in range(1, n + 1)])
+    if n and isinstance(n, int):
+        return sum([i**2 for i in range(1, n + 1)])
+    return None
