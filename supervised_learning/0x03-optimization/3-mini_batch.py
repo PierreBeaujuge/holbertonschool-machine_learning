8642fad4bfd9cc51c5817a111c2082031d6cebd6
PierreBeaujuge <pierre.beaujuge@gmail.com>
2020-05-22 14:09:01 +0000

    make shuffled copies of X_train, Y-train

--- supervised_learning/0x03-optimization/3-mini_batch.py
+++ supervised_learning/0x03-optimization/3-mini_batch.py
@@ -31,7 +31,7 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,
             print("\tValidation Accuracy: {}".format(acc_v))
 
             if epoch < epochs:
-                X_train, Y_train = shuffle_data(X_train, Y_train)
+                X_shuff, Y_shuff = shuffle_data(X_train, Y_train)
                 batches_float = X_train.shape[0] / batch_size
                 batches_int = int(X_train.shape[0] / batch_size)
                 step = 0
@@ -39,25 +39,25 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,
                 for i in range(0, batches_int):
                     step += 1
                     sess.run(train_op, feed_dict={
-                        x: X_train[i * batch_size: (i + 1) * batch_size],
-                        y: Y_train[i * batch_size: (i + 1) * batch_size]
+                        x: X_shuff[i * batch_size: (i + 1) * batch_size],
+                        y: Y_shuff[i * batch_size: (i + 1) * batch_size]
                     })
                     if step % 100 == 0:
                         loss_b = sess.run(loss, feed_dict={
-                            x: X_train[i * batch_size: (i + 1) * batch_size],
-                            y: Y_train[i * batch_size: (i + 1) * batch_size]
+                            x: X_shuff[i * batch_size: (i + 1) * batch_size],
+                            y: Y_shuff[i * batch_size: (i + 1) * batch_size]
                         })
                         acc_b = sess.run(accuracy, feed_dict={
-                            x: X_train[i * batch_size: (i + 1) * batch_size],
-                            y: Y_train[i * batch_size: (i + 1) * batch_size]
+                            x: X_shuff[i * batch_size: (i + 1) * batch_size],
+                            y: Y_shuff[i * batch_size: (i + 1) * batch_size]
                         })
                         print("Step {}:".format(step))
                         print("\tCost: {}".format(loss_b))
                         print("\tAccuracy: {}".format(acc_b))
                 if batches_float > batches_int:
                     sess.run(train_op, feed_dict={
-                        x: X_train[(i + 1) * batch_size:],
-                        y: Y_train[(i + 1) * batch_size:]
+                        x: X_shuff[(i + 1) * batch_size:],
+                        y: Y_shuff[(i + 1) * batch_size:]
                     })
 
         save_path = loader.save(sess, save_path)
