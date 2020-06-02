5fd63898c46176b622e25d562e110caf81684f9f
PierreBeaujuge <pierre.beaujuge@gmail.com>
2020-06-02 15:55:48 +0000

    init with empty callbacks array

--- supervised_learning/0x06-keras/6-train.py
+++ supervised_learning/0x06-keras/6-train.py
@@ -9,17 +9,18 @@ def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, early_stopping=False,
                 patience=0, verbose=True, shuffle=False):
     """function that trains a model using mini-batch gradient descent"""
-    early_stop = None
+    callbacks = []
     if validation_data and early_stopping:
         # The patience parameter is the number of epochs
         # upon which improvement should be checked
         early_stop = K.callbacks.EarlyStopping(monitor='val_loss',
                                                patience=patience)
+        callbacks.append(early_stop)
     history = network.fit(x=data, y=labels,
                           batch_size=batch_size,
                           epochs=epochs,
                           verbose=verbose,
                           shuffle=shuffle,
                           validation_data=validation_data,
-                          callbacks=[early_stop])
+                          callbacks=callbacks)
     return history
