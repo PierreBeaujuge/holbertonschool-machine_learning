8a13b270aee7c5743f1de98691ecdfa87a831ef7
PierreBeaujuge <pierre.beaujuge@gmail.com>
2020-06-02 15:08:53 +0000

    import tensorflow.keras as K

--- supervised_learning/0x06-keras/2-optimize.py
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -2,13 +2,13 @@
 """
 Optimize
 """
-import tensorflow as tf
+import tensorflow.keras as K
 
 
 def optimize_model(network, alpha, beta1, beta2):
     """function that sets up an Adam optimizer"""
-    optimizer = tf.keras.optimizers.Adam(lr=alpha,
-                                         beta_1=beta1, beta_2=beta2)
+    optimizer = K.optimizers.Adam(lr=alpha,
+                                  beta_1=beta1, beta_2=beta2)
     loss = 'categorical_crossentropy'
     network.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])
     return None
