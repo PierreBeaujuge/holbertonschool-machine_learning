0e8f3981190447b63ffcd8da92b6b7f6628a1e9c
PierreBeaujuge <pierre.beaujuge@gmail.com>
2020-06-02 15:07:07 +0000

    import tensorflow.keras as K

--- supervised_learning/0x06-keras/1-input.py
+++ supervised_learning/0x06-keras/1-input.py
@@ -2,19 +2,19 @@
 """
 Input - use the Input class
 """
-import tensorflow as tf
+import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """function that builds a neural network with the Keras library"""
-    inputs = tf.keras.layers.Input(shape=(nx,))
+    inputs = K.layers.Input(shape=(nx,))
     for i in range(len(layers)):
         if i == 0:
             outputs = inputs
-        outputs = tf.keras.layers.Dense(
+        outputs = K.layers.Dense(
             layers[i], activation=activations[i],
-            kernel_regularizer=tf.keras.regularizers.l2(lambtha))(outputs)
+            kernel_regularizer=K.regularizers.l2(lambtha))(outputs)
         if i != len(layers) - 1:
-            outputs = tf.keras.layers.Dropout(1 - keep_prob)(outputs)
-    network = tf.keras.models.Model(inputs=inputs, outputs=outputs)
+            outputs = K.layers.Dropout(1 - keep_prob)(outputs)
+    network = K.models.Model(inputs=inputs, outputs=outputs)
     return network
