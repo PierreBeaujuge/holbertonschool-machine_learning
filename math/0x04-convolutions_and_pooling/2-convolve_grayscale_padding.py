fecde8a084dda7685c434ec46793f1283fda4bb6
PierreBeaujuge <pierre.beaujuge@gmail.com>
2020-06-04 23:45:51 +0000

    pad images prior to convolution

--- math/0x04-convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/0x04-convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -15,18 +15,23 @@ def convolve_grayscale_padding(images, kernel, padding):
     image_num = np.arange(m)
     ph = padding[0]
     pw = padding[1]
-    output = np.zeros(shape=(m,
-                             h - kh + 1 + 2 * padding[0],
-                             w - kw + 1 + 2 * padding[1]))
 
-    for i in range(h - kh + 1):
-        for j in range(w - kw + 1):
+    # pad images before convolution, padding always symmetric here
+    padded_images = np.pad(images, pad_width=((0, 0), (ph, ph), (pw, pw)),
+                           mode='constant')
+    # output size depends on filter size and padding
+    output = np.zeros(shape=(m,
+                             h - kh + 1 + 2 * ph,
+                             w - kw + 1 + 2 * pw))
+
+    for i in range(h - kh + 1 + 2 * ph):
+        for j in range(w - kw + 1 + 2 * pw):
             output[
                 image_num,
-                i + ph,
-                j + pw
+                i,
+                j
             ] = np.sum(
-                images[
+                padded_images[
                     image_num,
                     i: i + kh,
                     j: j + kw
