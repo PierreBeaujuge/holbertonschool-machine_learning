cc56a937e1a659d00a14e376b269615fffb7d741
PierreBeaujuge <pierre.beaujuge@gmail.com>
2020-06-05 02:00:53 +0000

    revise padding='same' condition to handle padding and strides

--- math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
+++ math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
@@ -63,25 +63,19 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
                 )
         return output
     elif padding == 'same':
-        output = np.zeros(shape=(m,
-                                 int(h / sh),
-                                 int(w / sw)))
-        # pad images before convolution
-        # distinguish between even and odd filter sizes
-        if kh % 2 == 0:
-            ph = int(kh/2)
-        else:
-            ph = int((kh - 1)/2)
-        if kw % 2 == 0:
-            pw = int(kw/2)
-        else:
-            pw = int((kw - 1)/2)
-        # pad images accordingly, padding always symmetric here
+        ph = int(np.ceil(((sh * h) - sh + kh - h) / 2))
+        pw = int(np.ceil(((sw * w) - sw + kw - w) / 2))
+        # pad images before convolution, padding always symmetric here
         padded_images = np.pad(images, pad_width=((0, 0), (ph, ph), (pw, pw)),
                                mode='constant')
+        # output size depends on filter size and must be equal to image size
+        # imposing constraints on padding for a given set of strides
+        output = np.zeros(shape=(m,
+                                 int((h - kh + 2 * ph) / sh + 1),
+                                 int((w - kw + 2 * pw) / sw + 1)))
 
-        for i in range(int(h / sh)):
-            for j in range(int(w / sw)):
+        for i in range(int((h - kh + 2 * ph) / sh + 1)):
+            for j in range(int((w - kw + 2 * pw) / sw + 1)):
                 output[
                     image_num,
                     i,
