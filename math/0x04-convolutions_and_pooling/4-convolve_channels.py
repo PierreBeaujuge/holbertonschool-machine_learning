b30740e9bbbb3da938f3b2d7de84febe3aaa9d4c
PierreBeaujuge <pierre.beaujuge@gmail.com>
2020-06-05 02:39:15 +0000

    refactored code

--- math/0x04-convolutions_and_pooling/4-convolve_channels.py
+++ math/0x04-convolutions_and_pooling/4-convolve_channels.py
@@ -14,68 +14,42 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     kh = kernel.shape[0]
     kw = kernel.shape[1]
     image_num = np.arange(m)
-    ph = padding[0]
-    pw = padding[1]
     sh = stride[0]
     sw = stride[1]
 
     if isinstance(padding, tuple):
-        output = np.zeros(shape=(m,
-                                 int((h - kh + 2 * ph) / sh + 1),
-                                 int((w - kw + 2 * pw) / sw + 1)))
-        for i in range(int((h - kh) / sh + 1)):
-            for j in range(int((w - kw) / sw + 1)):
-                output[
-                    image_num,
-                    i + ph,
-                    j + pw
-                ] = np.sum(
-                    images[
-                        image_num,
-                        i * sh: i * sh + kh,
-                        j * sw: j * sw + kw,
-                        :
-                    ] * kernel,
-                    axis=(1, 2, 3)
-                )
-        return output
+        ph = padding[0]
+        pw = padding[1]
     elif padding == 'valid':
-        output = np.zeros(shape=(m,
-                                 int((h - kh) / sh + 1),
-                                 int((w - kw) / sw + 1)))
-        for i in range(int((h - kh) / sh + 1)):
-            for j in range(int((w - kw) / sw + 1)):
-                output[
-                    image_num,
-                    i,
-                    j
-                ] = np.sum(
-                    images[
-                        image_num,
-                        i * sh: i * sh + kh,
-                        j * sw: j * sw + kw,
-                        :
-                    ] * kernel,
-                    axis=(1, 2, 3)
-                )
-        return output
+        ph = 0
+        pw = 0
     elif padding == 'same':
-        output = np.zeros(shape=(m,
-                                 int(h / sh),
-                                 int(w / sw)))
-        for i in range(int((h - kh) / sh + 1)):
-            for j in range(int((w - kw) / sw + 1)):
-                output[
+        # output size depends on filter size and must be equal to image size
+        # imposing constraints on padding for a given set of strides
+        ph = int(np.ceil(((sh * h) - sh + kh - h) / 2))
+        pw = int(np.ceil(((sw * w) - sw + kw - w) / 2))
+
+    if isinstance(padding, tuple) or padding == 'same':
+        # pad images before convolution, padding always symmetric here
+        images = np.pad(images, pad_width=((0, 0), (ph, ph), (pw, pw)),
+                        mode='constant')
+
+    output = np.zeros(shape=(m,
+                             int((h - kh + 2 * ph) / sh + 1),
+                             int((w - kw + 2 * pw) / sw + 1)))
+
+    for i in range(int((h - kh + 2 * ph) / sh + 1)):
+        for j in range(int((w - kw + 2 * pw) / sw + 1)):
+            output[
+                image_num,
+                i,
+                j
+            ] = np.sum(
+                images[
                     image_num,
-                    i + int((kh - 1) / 2),
-                    j + int((kw - 1) / 2)
-                ] = np.sum(
-                    images[
-                        image_num,
-                        i * sh: i * sh + kh,
-                        j * sw: j * sw + kw,
-                        :
-                    ] * kernel,
-                    axis=(1, 2, 3)
-                )
-        return output
+                    i * sh: i * sh + kh,
+                    j * sw: j * sw + kw
+                ] * kernel,
+            axis=(1, 2, 3)
+            )
+    return output
