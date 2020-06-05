a9097bcaacf467e81336a0bf0404c8668a13ab03
PierreBeaujuge <pierre.beaujuge@gmail.com>
2020-06-05 03:11:08 +0000

    refactored code

--- math/0x04-convolutions_and_pooling/5-convolve.py
+++ math/0x04-convolutions_and_pooling/5-convolve.py
@@ -15,77 +15,45 @@ def convolve(images, kernels, padding='same', stride=(1, 1)):
     kw = kernels.shape[1]
     nc = kernels.shape[3]
     image_num = np.arange(m)
-    ph = padding[0]
-    pw = padding[1]
     sh = stride[0]
     sw = stride[1]
 
     if isinstance(padding, tuple):
-        output = np.zeros(shape=(m,
-                                 int((h - kh + 2 * ph) / sh + 1),
-                                 int((w - kw + 2 * pw) / sw + 1),
-                                 nc))
-        for k in range(nc):
-            for i in range(int((h - kh) / sh + 1)):
-                for j in range(int((w - kw) / sw + 1)):
-                    output[
-                        image_num,
-                        i + ph,
-                        j + pw,
-                        k
-                    ] = np.sum(
-                        images[
-                            image_num,
-                            i * sh: i * sh + kh,
-                            j * sw: j * sw + kw,
-                            :
-                        ] * kernels[:, :, :, k],
-                        axis=(1, 2, 3)
-                    )
-        return output
+        ph = padding[0]
+        pw = padding[1]
     elif padding == 'valid':
-        output = np.zeros(shape=(m,
-                                 int((h - kh) / sh + 1),
-                                 int((w - kw) / sw + 1),
-                                 nc))
-        for k in range(nc):
-            for i in range(int((h - kh) / sh + 1)):
-                for j in range(int((w - kw) / sw + 1)):
-                    output[
-                        image_num,
-                        i,
-                        j,
-                        k
-                    ] = np.sum(
-                        images[
-                            image_num,
-                            i * sh: i * sh + kh,
-                            j * sw: j * sw + kw,
-                            :
-                        ] * kernels[:, :, :, k],
-                        axis=(1, 2, 3)
-                    )
-        return output
+        ph = 0
+        pw = 0
     elif padding == 'same':
-        output = np.zeros(shape=(m,
-                                 int(h / sh),
-                                 int(w / sw),
-                                 nc))
-        for k in range(nc):
-            for i in range(int((h - kh) / sh + 1)):
-                for j in range(int((w - kw) / sw + 1)):
-                    output[
+        # output size depends on filter size and must be equal to image size
+        # imposing constraints on padding for a given set of strides
+        ph = int(np.ceil(((sh * h) - sh + kh - h) / 2))
+        pw = int(np.ceil(((sw * w) - sw + kw - w) / 2))
+
+    if isinstance(padding, tuple) or padding == 'same':
+        # pad images before convolution, padding always symmetric here
+        images = np.pad(images, pad_width=((0, 0), (ph, ph), (pw, pw), (0, 0)),
+                        mode='constant')
+
+    output = np.zeros(shape=(m,
+                             int((h - kh + 2 * ph) / sh + 1),
+                             int((w - kw + 2 * pw) / sw + 1),
+                             nc))
+
+    for k in range(nc):
+        for i in range(int((h - kh + 2 * ph) / sh + 1)):
+            for j in range(int((w - kw + 2 * pw) / sw + 1)):
+                output[
+                    image_num,
+                    i,
+                    j,
+                    k
+                ] = np.sum(
+                    images[
                         image_num,
-                        i + int((kh - 1) / 2),
-                        j + int((kw - 1) / 2),
-                        k
-                    ] = np.sum(
-                        images[
-                            image_num,
-                            i * sh: i * sh + kh,
-                            j * sw: j * sw + kw,
-                            :
-                        ] * kernels[:, :, :, k],
-                        axis=(1, 2, 3)
-                    )
-        return output
+                        i * sh: i * sh + kh,
+                        j * sw: j * sw + kw
+                    ] * kernels[:, :, :, k],
+                    axis=(1, 2, 3)
+                )
+    return output
