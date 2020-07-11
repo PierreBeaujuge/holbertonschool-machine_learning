db14f2f315fddaf9b5a755624251c69e932cdacc
PierreBeaujuge <pierre.beaujuge@gmail.com>
2020-07-11 07:39:08 +0000

    input answers

--- supervised_learning/0x0C-neural_style_transfer/10-neural_style.py
+++ supervised_learning/0x0C-neural_style_transfer/10-neural_style.py
@@ -322,8 +322,7 @@ class NST:
         # in a call to variational_cost()
         var_cost = self.variational_cost(generated_image)
 
-        total_cost = (self.alpha * content_cost + self.beta * style_cost +
-                      self.var * var_cost)
+        total_cost = (self.alpha * content_cost + self.beta * style_cost + self.var * var_cost)
 
         return (total_cost, content_cost, style_cost, var_cost)
 
@@ -403,14 +402,11 @@ class NST:
             # Compute the gradients and return the various costs
             # in a call to compute_grads()
             computed = self.compute_grads(generated_image)
-            gradients, total_cost, content_cost, style_cost, var_cost = (
-                computed)
+            gradients, total_cost, content_cost, style_cost, var_cost = computed
 
             # Print the costs at every "step"
             if i % step == 0 or i == iterations:
-                print("Cost at iteration {}: {}, content {}, style {}, var {}".
-                      format(i, total_cost, content_cost,
-                             style_cost, var_cost))
+                print("Cost at iteration {}: {}, content {}, style {}, var {}".format(i, total_cost, content_cost, style_cost, var_cost))
 
             # Backpropagation pass
             if i != iterations:
