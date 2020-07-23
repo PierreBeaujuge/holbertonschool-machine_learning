b6da17cda52ab2255ed346bb6fb45db20f297653
PierreBeaujuge <pierre.beaujuge@gmail.com>
2020-07-23 16:19:27 +0000

    input answers

--- math/0x05-advanced_linear_algebra/1-minor.py
+++ math/0x05-advanced_linear_algebra/1-minor.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""Advanced Linear Algebra"""
 
 
 def determinant(matrix):
--- math/0x05-advanced_linear_algebra/5-definiteness.py
+++ math/0x05-advanced_linear_algebra/5-definiteness.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-"""
-Definiteness
-"""
+"""Advanced Linear Algebra"""
 import numpy as np
 
 
