3d193536345bb7cb77bb014a55af3150a82cf97b
PierreBeaujuge <pierre.beaujuge@gmail.com>
2020-08-01 01:31:35 +0000

    removed filters

--- math/0x07-bayesian_prob/2-marginal.py
+++ math/0x07-bayesian_prob/2-marginal.py
@@ -68,24 +68,6 @@ def marginal(x, n, P, Pr):
     function that calculates the marginal probability of obtaining the data
     """
 
-    if not isinstance(n, int) or n < 1:
-        raise ValueError("n must be a positive integer")
-    if not isinstance(x, int) or x < 0:
-        err = "x must be an integer that is greater than or equal to 0"
-        raise ValueError(err)
-    if x > n:
-        raise ValueError("x cannot be greater than n")
-    if not isinstance(P, np.ndarray) or P.ndim != 1:
-        raise TypeError("P must be a 1D numpy.ndarray")
-    if not isinstance(Pr, np.ndarray) or P.shape != Pr.shape:
-        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
-    if not all(P >= 0) or not all(P <= 1):
-        raise ValueError("All values in P must be in the range [0, 1]")
-    if not all(Pr >= 0) or not all(Pr <= 1):
-        raise ValueError("All values in Pr must be in the range [0, 1]")
-    if not np.isclose([np.sum(Pr)], [1])[0]:
-        raise ValueError("Pr must sum to 1")
-
     # Calculate the marginal probability by summing over
     # all the possible conditions (here, the probabilities P)
     IN = intersection(x, n, P, Pr)
