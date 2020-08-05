916aeef30ea6112b88ddff82c3d2d6d82ed2dba1
PierreBeaujuge <pierre.beaujuge@gmail.com>
2020-08-05 18:58:02 +0000

    move if condition on X being an array of ints upfront

--- unsupervised_learning/0x01-clustering/1-kmeans.py
+++ unsupervised_learning/0x01-clustering/1-kmeans.py
@@ -8,14 +8,15 @@ import numpy as np
 def initialize(X, k):
     """function that initializes cluster centroids for K-means"""
 
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
+        return None
+
     # n: number of dada points
     # d: dimension of each data point
     n, d = X.shape
     # print(X.shape)
     # print(X)
 
-    if not isinstance(X, np.ndarray) or X.ndim != 2:
-        return None
     if not isinstance(k, int) or k <= 0 or k >= n:
         return None
 
