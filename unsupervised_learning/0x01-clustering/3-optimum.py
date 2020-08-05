07f2c816d7fb97445710f0acf5ddc623cfd835c0
PierreBeaujuge <pierre.beaujuge@gmail.com>
2020-08-05 23:07:40 +0000

    some odd error raised; framed script in try/except

--- unsupervised_learning/0x01-clustering/3-optimum.py
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -10,33 +10,33 @@ variance = __import__('2-variance').variance
 def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     """function that tests for the optimum number of clusters by variance"""
 
-    if not isinstance(X, np.ndarray) or X.ndim != 2:
-        return None, None
-
-    # n: number of dada points
-    # d: dimension of each data point
-    n, d = X.shape
-
-    if kmax is None:
-        kmax = n
-    if not isinstance(kmin, int) or kmin <= 0 or n <= kmin:
-        return None, None
-    if not isinstance(kmax, int) or kmax <= 0 or n < kmax:
-        return None, None
-    if kmin >= kmax:
-        return None, None
-    if not isinstance(iterations, int) or iterations <= 0:
-        return None, None
-
-    # Initialize list of tuples (C, clss)
-    results = []
-    # Initialize list of total intra-cluster variances
-    variances = []
-    # Initialize list of difference in variance from
-    # the smallest cluster size for each cluster size
-    d_vars = []
-
     try:
+        if not isinstance(X, np.ndarray) or X.ndim != 2:
+            return None, None
+
+        # n: number of dada points
+        # d: dimension of each data point
+        n, d = X.shape
+
+        if kmax is None:
+            kmax = n
+        if not isinstance(kmin, int) or kmin <= 0 or n <= kmin:
+            return None, None
+        if not isinstance(kmax, int) or kmax <= 0 or n < kmax:
+            return None, None
+        if kmin >= kmax:
+            return None, None
+        if not isinstance(iterations, int) or iterations <= 0:
+            return None, None
+
+        # Initialize list of tuples (C, clss)
+        results = []
+        # Initialize list of total intra-cluster variances
+        variances = []
+        # Initialize list of difference in variance from
+        # the smallest cluster size for each cluster size
+        d_vars = []
+
         # Iterate over the number of clusters under consideration
         for k in range(kmin, kmax+1):
 
@@ -52,7 +52,7 @@ def optimum_k(X, kmin=1, kmax=None, iterations=1000):
         for var in variances:
             d_vars.append(variances[0] - var)
 
+        return results, d_vars
+
     except Exception:
         return None, None
-
-    return results, d_vars
