968b2dd989c5209e3fd72a555f395c532f816aad
PierreBeaujuge <pierre.beaujuge@gmail.com>
2020-08-09 22:26:56 +0000

    update main files

--- unsupervised_learning/0x01-clustering/8-main.py
+++ unsupervised_learning/0x01-clustering/8-main.py
@@ -3,6 +3,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 expectation_maximization = __import__('8-EM').expectation_maximization
+EM = __import__('8-EM').expectation_maximization
 
 if __name__ == '__main__':
     np.random.seed(11)
@@ -22,3 +23,33 @@ if __name__ == '__main__':
     print(m)
     print(S)
     print(l)
+
+    # np.random.seed(11)
+    # a = np.random.multivariate_normal([30, 40], [[75, 5], [5, 75]], size=10000)
+    # b = np.random.multivariate_normal([5, 25], [[16, 10], [10, 16]], size=750)
+    # c = np.random.multivariate_normal([60, 30], [[16, 0], [0, 16]], size=750)
+    # d = np.random.multivariate_normal([20, 70], [[35, 10], [10, 35]], size=1000)
+    # X = np.concatenate((a, b, c, d), axis=0)
+    # np.random.shuffle(X)
+    # k = 4
+    # pi, m, S, g, l = EM(X, k, iterations=50, tol=1e-6, verbose=True)
+    # print(pi)
+    # print(m)
+    # print(S)
+    # print(g)
+    # print(l.round(6))
+
+    # np.random.seed(11)
+    # a = np.random.multivariate_normal([30, 40], [[75, 5], [5, 75]], size=10000)
+    # b = np.random.multivariate_normal([5, 25], [[16, 10], [10, 16]], size=750)
+    # c = np.random.multivariate_normal([60, 30], [[16, 0], [0, 16]], size=750)
+    # d = np.random.multivariate_normal([20, 70], [[35, 10], [10, 35]], size=1000)
+    # X = np.concatenate((a, b, c, d), axis=0)
+    # np.random.shuffle(X)
+    # k = 4
+    # pi, m, S, g, l = EM(X, k, verbose=False)
+    # print(pi)
+    # print(m)
+    # print(S)
+    # print(g)
+    # print(l.round(5))
