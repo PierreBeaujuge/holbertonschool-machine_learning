397e2e3bfe7d4399128d60e7f982e200d8ff49cc
PierreBeaujuge <pierre.beaujuge@gmail.com>
2020-09-25 22:36:50 +0000

    revised axis=1 handling

--- supervised_learning/0x11-attention/1-self_attention.py
+++ supervised_learning/0x11-attention/1-self_attention.py
@@ -18,11 +18,14 @@ class SelfAttention(tf.keras.layers.Layer):
     def call(self, s_prev, hidden_states):
         """function that builds the self-attention layer"""
 
-        W = self.W(s_prev)[:, tf.newaxis, :]
+        s_prev_with_time_axis = tf.expand_dims(s_prev, 1)
+
+        W = self.W(s_prev_with_time_axis)
         U = self.U(hidden_states)
-        V = self.V(tf.nn.tanh(W + U))
+        score = self.V(tf.nn.tanh(W + U))
 
-        weights = tf.nn.softmax(V, axis=1)
-        context = tf.reduce_sum((weights * hidden_states), axis=1)
+        attention_weights = tf.nn.softmax(score, axis=1)
+        context_vector = tf.reduce_sum((attention_weights * hidden_states),
+                                       axis=1)
 
-        return context, weights
+        return context_vector, attention_weights
